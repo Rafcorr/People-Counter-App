f154f95f01bf087c586a005884871f0934c90ac4
Rafael Corrêa <rafalmcorrea@gmail.com>
2022-08-06 20:10:39 -0300

    adds buttons and basic behavior

--- app/src/main/java/com/example/peoplecounter/MainActivity.kt
+++ app/src/main/java/com/example/peoplecounter/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.peoplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val addButton = findViewById<Button>(R.id.add_btn)
+        val removeButton = findViewById<Button>(R.id.remove_btn)
+
+        addButton.setOnClickListener {
+            textView.text = "Add button clicked"
+        }
+
+        removeButton.setOnClickListener {
+            textView.text = "Remove button clicked"
+        }
     }
 }
